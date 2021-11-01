bf6776af299450a2e331268bf57b2f7c2ee6193a
Aryan5752 <82256448+Aryan5752@users.noreply.github.com>
2021-11-01 17:36:35 +0530

    Add files via upload

---
+++ a9.js
@@ -0,0 +1,15 @@
+var a;
+console.log(a);
+console.log(x);
+if (a==4) {
+	console.log("a is defined");
+}
+
+a=5;
+if (a==4) {
+	console.log("a is defined");
+}
+else{
+console.log("a is not undefined");	
+}
+
